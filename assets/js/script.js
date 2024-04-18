5d741e48d52b88ec82050151df5e711149abfa04
Sorokin Denis <uuuiiiii.v@gmail.com>
2024-04-18 20:40:09 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,386 @@
+const peopleArray = [{
+    id: 1,
+    name: "Mark Zuckerberg",
+    age: 38,
+    photo: 'assets/img/photo-MarkZuckerberg.png',
+    gender: 'male',
+},
+{
+    id: 2,
+    name: "Claire Boucher",
+    age: 35,
+    photo: 'assets/img/photo-ClaireBoucher.png',
+    gender: 'female',
+},
+{
+    id: 3,
+    name: "Ty Simpkins",
+    age: 21,
+    photo: 'assets/img/photo-TySimpkins.png',
+    gender: 'male',
+},
+{
+    id: 4,
+    name: "Tom Holland",
+    age: 26,
+    photo: 'assets/img/photo-TomHolland.png',
+    gender: 'male',
+},
+{
+    id: 5,
+    name: "Bill Gates",
+    age: 67,
+    photo: 'assets/img/photo-BillGates.png',
+    gender: 'male',
+},
+{
+    id: 6,
+    name: "Elon Musk",
+    age: 51,
+    photo: 'assets/img/photo-ElonMusk.png',
+    gender: 'male',
+},
+{
+    id: 7,
+    name: "Talulah Jane",
+    age: 37,
+    photo: 'assets/img/photo-TalulahJane.png',
+    gender: 'female',
+},
+{
+    id: 8,
+    name: "Bernard Arnault",
+    age: 74,
+    photo: 'assets/img/photo-BernardArnault.png',
+    gender: 'male',
+},
+{
+    id: 9,
+    name: "Jeff Bezos",
+    age: 59,
+    photo: 'assets/img/photo-JeffBezos.png',
+    gender: 'male',
+},
+{
+    id: 10,
+    name: "Tosca Musk",
+    age: 48,
+    photo: 'assets/img/photo-ToscaMusk.png',
+    gender: 'female',
+},
+{
+    id: 11,
+    name: "Steve Jobs",
+    age: 56,
+    photo: 'assets/img/photo-SteveJobs.png',
+    gender: 'male',
+},
+{
+    id: 12,
+    name: "Amber Heard",
+    age: 36,
+    photo: 'assets/img/photo-AmberHeard.png',
+    gender: 'female',
+},
+{
+    id: 13,
+    name: "Laurene Powell",
+    age: 59,
+    photo: 'assets/img/photo-LaurenePowell.png',
+    gender: 'female',
+},
+{
+    id: 14,
+    name: "Eve Jobs",
+    age: 24,
+    photo: 'assets/img/photo-EveJobs.png',
+    gender: 'female',
+},
+{
+    id: 15,
+    name: "Chrisann Brennan",
+    age: 68,
+    photo: 'assets/img/photo-ChrisannBrennan.png',
+    gender: 'female',
+},
+{
+    id: 16,
+    name: "Cara Delevingne",
+    age: 30,
+    photo: 'assets/img/photo-CaraDelevingne.png',
+    gender: 'female',
+},
+{
+    id: 17,
+    name: "Cam Gigandet",
+    age: 40,
+    photo: 'assets/img/photo-CamGigandet.png',
+    gender: 'male',
+},
+{
+    id: 18,
+    name: "Sean Faris",
+    age: 41,
+    photo: 'assets/img/photo-SeanFaris.png',
+    gender: 'male',
+},
+{
+    id: 19,
+    name: "Penn Badgley",
+    age: 41,
+    photo: 'assets/img/photo-PennBadgley.png',
+    gender: 'male',
+},
+{
+    id: 20,
+    name: "Camille Vasquez",
+    age: 38,
+    photo: 'assets/img/photo-CamilleVasquez.png',
+    gender: 'female',
+},
+{
+    id: 21,
+    name: "Margot Robbie",
+    age: 32,
+    photo: 'assets/img/photo-MargotRobbie.png',
+    gender: 'female',
+},
+{
+    id: 22,
+    name: "Scarlett Johansson",
+    age: 38,
+    photo: 'assets/img/photo-ScarlettJohansson.png',
+    gender: 'female',
+},
+{
+    id: 23,
+    name: "Angelina Jolie",
+    age: 47,
+    photo: 'assets/img/photo-AngelinaJolie.png',
+    gender: 'female',
+},
+{
+    id: 24,
+    name: "Gal Gadot",
+    age: 37,
+    photo: 'assets/img/photo-GalGadot.png',
+    gender: 'female',
+},
+{
+    id: 25,
+    name: "Vito Schnabel",
+    age: 36,
+    photo: 'assets/img/photo-VitoSchnabel.png',
+    gender: 'male',
+},
+{
+    id: 26,
+    name: "Dylan Minnette",
+    age: 26,
+    photo: 'assets/img/photo-DylanMinnette.png',
+    gender: 'male',
+},
+{
+    id: 27,
+    name: "Ansel Elgort",
+    age: 29,
+    photo: 'assets/img/photo-AnselElgort.png',
+    gender: 'male',
+},
+{
+    id: 28,
+    name: "Timothée Chalamet",
+    age: 27,
+    photo: 'assets/img/photo-TimotheeChalame.png',
+    gender: 'male',
+},
+{
+    id: 29,
+    name: "Josh Hutcherson",
+    age: 30,
+    photo: 'assets/img/photo-JoshHutcherson.png',
+    gender: 'male',
+},
+{
+    id: 30,
+    name: "Aimee Wood",
+    age: 29,
+    photo: 'assets/img/photo-AimeeWood.png',
+    gender: 'female',
+},
+{
+    id: 31,
+    name: "Chloë Moretz",
+    age: 26,
+    photo: 'assets/img/photo-ChloeMoretz.png',
+    gender: 'female',
+},
+{
+    id: 32,
+    name: "Mimi Keene",
+    age: 24,
+    photo: 'assets/img/photo-MimiKeene.png',
+    gender: 'female',
+},
+{
+    id: 33,
+    name: "Elle Fanning",
+    age: 25,
+    photo: 'assets/img/photo-ElleFanning.png',
+    gender: 'female',
+},
+{
+    id: 34,
+    name: "Dakota Fanning",
+    age: 29,
+    photo: 'assets/img/photo-DakotaFanning.png',
+    gender: 'female',
+},
+{
+    id: 35,
+    name: "Justice Smith",
+    age: 27,
+    photo: 'assets/img/photo-JusticeSmith.png',
+    gender: 'male',
+},
+{
+    id: 36,
+    name: "Isabella Sermon",
+    age: 16,
+    photo: 'assets/img/photo-IsabellaSermon.png',
+    gender: 'female',
+},
+{
+    id: 37,
+    name: "Omar Sy",
+    age: 45,
+    photo: 'assets/img/photo-OmarSy.png',
+    gender: 'male',
+},
+]
+
+window.addEventListener('load', function(){
+    const userContainer = this.document.querySelector('.user-container');
+    const nameInput = this.document.querySelector('#nameFilter');
+    const ageRange = this.document.querySelector('#ageRange');
+    const genderRadio = this.document.querySelectorAll('.gender-radio');
+    
+
+    function findMinAge (arr) {
+        let minAge = Math.min(...arr.map(item => item.age))
+
+        return minAge
+    }
+    
+    function findMaxAge (arr) {
+        let maxAge = Math.max(...arr.map(item => item.age))
+
+        return maxAge
+    }
+
+    let minAge = findMinAge(peopleArray)
+    let maxAge = findMaxAge(peopleArray)
+
+    let config = {
+        gender: 'all',
+        age: '120',
+        name: '',
+        rangeValue: '40',
+        rangeMin: minAge,
+        rangeMax: maxAge
+    }
+
+    const setAgeFilter = () => {
+        ageRange.setAttribute('value', config.rangeValue)
+        ageRange.setAttribute('min', config.rangeMin)
+        ageRange.setAttribute('max', config.rangeMax)
+    }
+
+    setAgeFilter()
+
+    const renderPerson = (array) => {
+        const list = array.map(user => {
+            return (`<div class="user">
+               <div class="user-photo">
+                   <img src="${user.photo}" alt="">
+               </div>
+               <div class="user-name">${user.name}</div>
+               <div class="user-age">${user.age}</div>
+               <div class="user-gender">${user.gender}</div>
+           </div>`)
+        }).join('');
+
+        userContainer.innerHTML = list;
+    }
+    renderPerson(peopleArray);
+
+
+    
+    function getNameValue(){
+        nameInput.addEventListener('input', () => {
+            let value = nameInput.value.toLowerCase().replace(/\s+/g, ' ').trim();
+            config.name = value;
+
+            const filteredForName = mainFilter();
+            renderPerson(filteredForName);
+        })
+    }
+
+    getNameValue();
+
+
+
+    function getAgeValue () {
+        ageRange.addEventListener('input', () => {
+            let value = ageRange.value;
+            config.age = value;
+
+            // const filteredForAge = ageFilter();
+            const filteredForAge = mainFilter();
+            renderPerson(filteredForAge);
+        })
+    }
+
+    getAgeValue();
+
+
+
+
+    function getGenderValue () {
+        genderRadio.forEach(item =>{
+            item.addEventListener('click', ()=>{
+                let value = item.value;
+                config.gender = value;
+                // const filteredForGender = genderFilter();
+                const filteredForGender = mainFilter();
+                renderPerson(filteredForGender);
+            })
+        })
+    }
+
+    getGenderValue();
+
+    // function genderFilter () {
+    //     return ( config.gender === "all" ? peopleArray : peopleArray.filter(item => item.gender === config.gender))
+    // }
+
+    // function ageFilter () {
+    //     return peopleArray.filter(item => item.age <= config.age)
+    // }
+
+    // function nameFilter () {
+    //     return peopleArray.filter(item => item.name.toLowerCase().includes(config.name))
+    // }
+
+    function mainFilter () {
+        const filtered = peopleArray.filter(item => {
+            const setName = item.name.toLowerCase().includes(config.name)
+            const setAge = item.age <= config.age;
+            const setGender = config.gender === "all" ? peopleArray : item.gender === config.gender
+
+            return setName && setAge && setGender;
+            
+        })
+        return filtered;
+    }
+})
